085588be26157fee44baa2a227c8f2d3ff42f092
Syed Muneeb Kazmi <syed.kazmi47@gmail.com>
2015-08-20 14:59:24 +0100

    Designed the login view on storyboard

---
+++ Employee Bio App/Employee Bio App/BioDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  BioDetailViewController.swift
+//  Employee Bio App
+//
+//  Created by Syed Muneeb Kazmi on 20/08/2015.
+//  Copyright (c) 2015 Knowledge Management & Transfer. All rights reserved.
+//
+
+import UIKit
+
+class BioDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
